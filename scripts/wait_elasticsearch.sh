ffa98ab97fd220c6b92232054a2f7116bfcb2f7e
Charles <18oliveira.charles@gmail.com>
2019-03-12 17:24:21 -0300

    Improved detection of Elasticsearch readiness

--- scripts/wait_elasticsearch.sh
+++ scripts/wait_elasticsearch.sh
@@ -1,15 +1,17 @@
 #!/bin/bash
 
+PORT=${PORT:-9200}
 attempts=0
 while [ $attempts != 60 ]
 do 
-    curl -s localhost:9200 -o - > /dev/null
-    if [ "$?" == "0" ]
-    then 
+    ack=$(curl -s -X POST localhost:$PORT/dummy -o - || echo down)
+    if [[ "$ack" =~ .*dummy.* ]]
+    then
+        curl -s -X DELETE localhost:$PORT/dummy -o - > /dev/null
         attempts=0
         break
     fi
     sleep 2
-    ((attempts=$attempts + 1))
+    ((attempts++))
 done
 exit $attempts
